282be6bf14f25d483a7948e1f2d1b39c3e9c9117
SeraLee <cieroyou1@gmail.com>
2020-03-26 17:46:29 +0900

    함수에 type 형식 설정하기

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
-const myname = "Sera"
-const age = 30, gender = "femail";
+const myname = "Sera",age = 30, gender = "femail";
 
-const sayHi = (name, age, gender?) => {
+// const sayHi = (name:string, age:number, gender:string) => {
+//     console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
+// }
+
+const sayHi = (name:string, age:number, gender:string) : void =>{
     console.log(`Hello ${name}, you are ${age}, you are ${gender}`);
 }
-
 //gender? 물음표가 없을 경우 'An argument for 'gender' was not provided' 에러가 발생
-sayHi(myname,age);
+sayHi(myname,age,gender);
 
 export {};
 
